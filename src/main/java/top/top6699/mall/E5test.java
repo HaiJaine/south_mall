b86ced48c02aa800392b02bcf123b3c32dd70854
HaiJaine <2215654043@qq.com>
2022-07-10 16:10:27 +0800

    保持GitHub活跃

--- src/main/java/top/top6699/mall/E5test.java
+++ src/main/java/top/top6699/mall/E5test.java
@@ -6,4 +6,10 @@ package top.top6699.mall;
  * @description 本类只是为了保持GitHub活跃，无任何意义
  **/
 public class E5test {
+
+
+    // 活跃GitHub
+    public static void main(String[] args) {
+        System.out.println("活跃GitHub");
+    }
 }
